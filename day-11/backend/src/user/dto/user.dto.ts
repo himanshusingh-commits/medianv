4d30688b8c8c7badde0dbaf093dea3282a7d4ae2
Himanshu singh <himanshusingh@medianv.com>
2026-01-07 23:10:45 +0530

    Added Swagger

--- day-11/backend/src/user/dto/user.dto.ts
+++ day-11/backend/src/user/dto/user.dto.ts
@@ -1,7 +1,9 @@
 import { MinLength } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class UserDto {
+  @ApiProperty({ description: 'The name of the user', example: 'John Doe' })
   @MinLength(3)
   name: string;
+  @ApiProperty({ description: 'The email of the user', example: 'crickter' })
   type: string;
 }
--- day-11/backend/src/user/user.controller.ts
+++ day-11/backend/src/user/user.controller.ts
@@ -9,17 +9,22 @@ import {
   Post,
 } from '@nestjs/common';
 import { UserService } from './user.service';
+import { ApiTags, ApiResponse, ApiOperation } from '@nestjs/swagger';
 import { UserDto } from './dto/user.dto';
+
+@ApiTags('users')
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: 'getting all data ' })
   getAllUsers() {
     return this.userService.getAllUsers();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'For gettring data' })
   getUsers(@Param('id', ParseIntPipe) id: number) {
     try {
       return this.userService.getUsers(id);
@@ -33,11 +38,14 @@ export class UserController {
   }
 
   @Post()
+  @ApiOperation({ summary: 'Create a new user' })
+  @ApiResponse({ status: 201, description: 'User created successfully.' })
   addUser(@Body() userDto: UserDto) {
     return this.userService.addUser(userDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'deleete all dataa' })
   deleteUser(@Param('id', ParseIntPipe) id: number) {
     return this.userService.deleteUser(id);
   }
